03adb463f15e8f7cc2243d9861e3b15fe9337dd3
ahmadfauzanisnata <fauzanisnata56@gmail.com>
2024-03-28 15:11:12 +0700

    membuat step 1

--- pertemuan keenam_QuickSort/pertemuan keenam_QuickSort.cpp
+++ pertemuan keenam_QuickSort/pertemuan keenam_QuickSort.cpp
@@ -45,6 +45,13 @@ void swap(int x, int y)
     mov_count++;
 }
 void q_short(int low, int high)
+{
+    int temp;
+    int pivot, i, j;
+    if (low > high) { //step 1
+        return;
+    }
+}
 
 int main()
 
