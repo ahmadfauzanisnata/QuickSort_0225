b46fc788ee92eb224ff4353cbba4b918cfa34818
ahmadfauzanisnata <fauzanisnata56@gmail.com>
2024-03-28 14:44:55 +0700

    membuat bilangan bulat untuk menyimpan nilai

--- pertemuan keenam_QuickSort/pertemuan keenam_QuickSort.cpp
+++ pertemuan keenam_QuickSort/pertemuan keenam_QuickSort.cpp
@@ -4,6 +4,11 @@
 #include <iostream>
 using namespace std;
 
+//array of integers to hold values
+int arr[20];
+int cmp_count = 0; // number of comparison
+int mov_count = 0; // number of data movement
+int n;
 int main()
 
 
